19b15275999a807474898c1a239be0fc08dd4275
Firefly718 <System.BLA.16@yandex.ru>
2020-04-11 16:33:28 +0300

    removed comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-// function toggleText () {
+//-------------------------------------------------------------Burger Menu
+var isMenuShow = false;
 
-// }
+burger.addEventListener("click", function() {
+    if (isMenuShow) {
+        document.getElementById('burger').src = "./img/burgerClose.png";
+        document.getElementById('bars').style.display = "flex";
+        isMenuShow = false;
+    } else {
+        document.getElementById('burger').src = "./img/burger.png";
+        document.getElementById('bars').style.display = "none";
+        isMenuShow  = true;
+    }
+});
+
+//-------------------------------------------------------------Slow Scroll
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
